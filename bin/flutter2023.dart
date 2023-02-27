7d969acfa00fc3f259f3d064867f209e078a8cb8
Usmanov Mansurjon <usmanovmansur@gmail.com>
2023-02-27 12:48:31 +0500

    Initial commit

---
+++ bin/flutter2023.dart
@@ -0,0 +1,5 @@
+import 'package:flutter2023/flutter2023.dart' as flutter2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter2023.calculate()}!');
+}
---
+++ test/flutter2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter2023/flutter2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
